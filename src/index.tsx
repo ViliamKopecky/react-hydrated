125c12f89d95bee4fe9609412d42625339a140b0
Viliam Kopecky <vk@viliamkopecky.cz>
2022-03-17 20:57:00 +0100

    react-hydrated init

---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const hydratedValues: Record<string, boolean | undefined> = {}
+
+const HydratingContext = React.createContext<null | boolean>(null)
+
+export function useHydrated() {
+	const value = React.useContext(HydratingContext)
+
+	if (value === null) {
+		throw new Error('Missing HydratingRoot context.')
+	}
+
+	return value
+}
+
+export const HydratingRoot = React.memo(function HydratingRoot(props) {
+	const [index] = React.useState(() => {
+		const g = global as { _hydratationIndex?: number }
+		g._hydratationIndex = (g._hydratationIndex ?? 0) + 1
+		return g._hydratationIndex
+	})
+
+	const [hydrated, setHydrated] = React.useState(() => hydratedValues[index] ?? false)
+
+	React.useEffect(() => {
+		const g = global as { _hydratationRecords?: Record<number, boolean> }
+		const records = (g._hydratationRecords = g._hydratationRecords ?? {})
+		records[index] = true
+		setHydrated(true)
+	}, [index])
+
+	return <HydratingContext.Provider value={hydrated}>{props.children}</HydratingContext.Provider>
+})
